d91eded8e35cb000f490dc8114ccf99752386369
Arun052003 <arun.sundar@digicoffer.com>
2023-08-14 16:21:23 +0530

    commiting a file for lauditor_file

---
+++ app/src/main/java/com/example/myapplication/lauditor_file.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lauditor_file extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lauditor_file);
+    }
+}
