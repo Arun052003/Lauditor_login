0b854506d933379e45b2aa25bb7bfb557963c167
Arun052003 <arun.sundar@digicoffer.com>
2023-08-16 09:28:29 +0530

    commiting a file for lauditor_file

---
+++ app/src/main/java/com/example/myapplication/validation.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class validation {
+    public boolean validate_password(String password)
+    {
+        if (password.equals("Arun123"))
+        {
+            return  true;
+        }
+        else
+        {
+            return  false;
+        }
+    }
+}
